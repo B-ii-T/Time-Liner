141dfc528c939615c240cd65dde1e593b41850f1
Abdelfatah <bnb.fatah@gmail.com>
2023-09-01 15:53:21 +0100

    Fix database cannot save Date type by creating converter class

--- app/src/main/java/com/bit/timeliner/AppDatabase.java
+++ app/src/main/java/com/bit/timeliner/AppDatabase.java
@@ -5,11 +5,13 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Database(entities = {Timeline.class, Deadline.class}, version = 1, exportSchema = false)
+@TypeConverters(DateConverter.class) // Add this line to register the DateConverter
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TimelineDAO timelineDAO();
     public abstract DeadlineDAO deadlineDAO();
