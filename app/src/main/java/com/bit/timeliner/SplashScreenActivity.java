78ed6c1bea649f9c77b9af333e900046b7c013a3
Abdelfatah <bnb.fatah@gmail.com>
2023-09-03 10:35:58 +0100

    Splash screen with lottie animation

---
+++ app/src/main/java/com/bit/timeliner/SplashScreenActivity.java
@@ -0,0 +1,42 @@
+package com.bit.timeliner;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        LottieAnimationView lottieAnimationView = findViewById(R.id.splash_logo_lottie);
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(@NonNull Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(@NonNull Animator animation) {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationCancel(@NonNull Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(@NonNull Animator animation) {
+
+            }
+        });
+    }
+}
